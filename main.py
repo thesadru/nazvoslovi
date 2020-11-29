aca301914f1043da21dafcaef9ee61a4fca456fb
Daniel <dan0.suman@gmail.com>
2020-11-29 12:19:15 +0100

    Fixed json output
    am dumb lmao.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from nazvoslovi import pprint, recognize, NazvosloviException, COMPOUNDS
+from nazvoslovi import BaseCompound, pprint, recognize, NazvosloviException, COMPOUNDS
 import sys
 import json
 
@@ -7,18 +7,18 @@ if len(sys.argv)==1:
     do_json = False
 else:
     if sys.argv[1] == 'help':
-        print('známé sloučeniny: '+', '.join(c.typename for c in COMPOUNDS))
-        quit()
+        quit(print('známé sloučeniny: '+', '.join(c.typename for c in COMPOUNDS)))
     do_json = '--json' in sys.argv
     entry = sys.argv[1]
 
 compound = recognize(entry)
-if compound is None:
-    print(f'sloučenina "{entry}" nebyla rozpoznána')
-elif isinstance(compound,NazvosloviException):
-    print(compound)
+if isinstance(compound,NazvosloviException):
+    quit(print(compound))
+
+if do_json:
+    print(json.dumps(compound.todict() if compound is BaseCompound else None))
 else:
-    if do_json:
-        print(json.dumps(compound.todict()))
+    if compound is None:
+        print(f'sloučenina "{entry}" nebyla rozpoznána')
     else:
         print('\n'+pprint(compound,entry))
